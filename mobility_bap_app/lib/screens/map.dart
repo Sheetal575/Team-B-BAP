6b820607be0de061592ad7be9ab3373d243f6728
Sheetal575 <sheetaldadh9@gmail.com>
2021-07-02 10:52:37 +0530

    changes in map

--- mobility_bap_app/lib/screens/map.dart
+++ mobility_bap_app/lib/screens/map.dart
@@ -34,7 +34,7 @@ class _MyMapState extends State<MyMap> {
 
   final startAddressFocusNode = FocusNode();
   final desrinationAddressFocusNode = FocusNode();
-
+  late BitmapDescriptor startCustomIcon;     
   String _startAddress = '';
   String _destinationAddress = '';
   String? _placeDistance;
@@ -54,16 +54,16 @@ Map<PolylineId, Polyline> polylines = {};
         _currentPosition = position;
         print('CURRENT POS: $_currentPosition');
          // Start Location Marker
-        Marker currentLocationMarker = Marker(
-          markerId: MarkerId('currentLocationMarker'),
-          position: LatLng(position.latitude, position.longitude),
-          infoWindow: InfoWindow(
-            title: 'Current Location',
-            // snippet: _startAddress,
-          ),
-          icon: BitmapDescriptor.defaultMarker,
-        );
-        markers.add(currentLocationMarker);
+        // Marker currentLocationMarker = Marker(
+        //   markerId: MarkerId('currentLocationMarker'),
+        //   position: LatLng(position.latitude, position.longitude),
+        //   infoWindow: InfoWindow(
+        //     title: 'Current Location',
+        //     // snippet: _startAddress,
+        //   ),
+        //   icon: BitmapDescriptor.defaultMarker,
+        // );
+        // markers.add(currentLocationMarker);
         mapController.animateCamera(
           CameraUpdate.newCameraPosition(
             CameraPosition(
@@ -101,12 +101,73 @@ Map<PolylineId, Polyline> polylines = {};
     }
   }
 
+  //to add custom marker
+  void setCustomMarker() async {
+    startCustomIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(devicePixelRatio: 2.5), 'assets/images/current_loc.png');
+  }
+ 
+_calculateDistance() async {
+   List<Location> startPlacemark = await locationFromAddress(_startAddress);
+    List<Location> destinationPlacemark = await locationFromAddress(_destinationAddress);
+
+double startLatitude = _startAddress == _currentAddress
+          ? _currentPosition.latitude
+          : startPlacemark[0].latitude;
+
+      double startLongitude = _startAddress == _currentAddress
+          ? _currentPosition.longitude
+          : startPlacemark[0].longitude;
+      double destinationLatitude = destinationPlacemark[0].latitude;
+      double destinationLongitude = destinationPlacemark[0].longitude;
+
+      String startCoordinatesString = '($startLatitude, $startLongitude)';
+      String destinationCoordinatesString =
+          '($destinationLatitude, $destinationLongitude)';
+          
+       setState(() {
+         
+        
+      // Start Location Marker
+      Marker startMarker = Marker(
+        
+        markerId: MarkerId('startCoordinatesString'),
+        position: LatLng(startLatitude, startLongitude),
+        infoWindow: InfoWindow(
+          title: 'Start $startCoordinatesString',
+          snippet: _startAddress,
+        ),
+        icon:  BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueCyan),
+      );
+
+      // Destination Location Marker
+      Marker destinationMarker = Marker(
+        markerId: MarkerId('destinationCoordinatesString'),
+        position: LatLng(destinationLatitude, destinationLongitude),
+        infoWindow: InfoWindow(
+          title: 'Destination $destinationCoordinatesString',
+          snippet: _destinationAddress,
+        ),
+        icon:  BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
+      );
+
+       markers.add(startMarker);
+      markers.add(destinationMarker);
+});
+      print(
+        'START COORDINATES: ($startLatitude, $startLongitude)',
+      );
+      print(
+        'DESTINATION COORDINATES: ($destinationLatitude, $destinationLongitude)',
+      );
+}
 
 
   @override
     void initState() {
     super.initState();
        _getCurrentLocation();
+       setCustomMarker();
   }
 
 
@@ -237,101 +298,103 @@ Map<PolylineId, Polyline> polylines = {};
             ),
             // Show the place input fields & button for
             // showing the route
-            // SafeArea(
-            //   child: Align(
-            //     alignment: Alignment.topCenter,
-            //     child: Padding(
-            //       padding: const EdgeInsets.only(top: 10.0),
-            //       child: Container(
-            //         decoration: BoxDecoration(
-            //           color: Colors.white70,
-            //           borderRadius: BorderRadius.all(
-            //             Radius.circular(20.0),
-            //           ),
-            //         ),
-            //         width: width * 0.9,
-            //         child: Padding(
-            //           padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-            //           child: Column(
-            //             mainAxisSize: MainAxisSize.min,
-            //             children: <Widget>[
-            //               Text(
-            //                 'Places',
-            //                 style: TextStyle(fontSize: 20.0),
-            //               ),
-            //               SizedBox(height: 10),
-            //               _textField(
-            //                   label: 'Start',
-            //                   hint: 'Choose starting point',
-            //                   prefixIcon: Icon(Icons.looks_one),
-            //                   suffixIcon: IconButton(
-            //                     icon: Icon(Icons.my_location),
-            //                     onPressed: () {
-            //                       startAddressController.text = _currentAddress;
-            //                       _startAddress = _currentAddress;
-            //                     },
-            //                   ),
-            //                   controller: startAddressController,
-            //                   focusNode: startAddressFocusNode,
-            //                   width: width,
-            //                   locationCallback: (String value) {
-            //                     setState(() {
-            //                       _startAddress = value;
-            //                     });
-            //                   }),
-            //               SizedBox(height: 10),
-            //               _textField(
-            //                   label: 'Destination',
-            //                   hint: 'Choose destination',
-            //                   prefixIcon: Icon(Icons.looks_two),
-            //                   controller: destinationAddressController,
-            //                   focusNode: desrinationAddressFocusNode,
-            //                   width: width,
-            //                   locationCallback: (String value) {
-            //                     setState(() {
-            //                       _destinationAddress = value;
-            //                     });
-            //                   }),
-            //               SizedBox(height: 10),
-            //               Visibility(
-            //                 visible: _placeDistance == null ? false : true,
-            //                 child: Text(
-            //                   'DISTANCE: $_placeDistance km',
-            //                   style: TextStyle(
-            //                     fontSize: 16,
-            //                     fontWeight: FontWeight.bold,
-            //                   ),
-            //                 ),
-            //               ),
-            //               SizedBox(height: 5),
-            //               ElevatedButton(
-            //                 onPressed: (){
-            //                   showPlaces();
-            //                   },
-            //                 child: Padding(
-            //                   padding: const EdgeInsets.all(8.0),
-            //                   child: Text(
-            //                     'Show Route'.toUpperCase(),
-            //                     style: TextStyle(
-            //                       color: Colors.white,
-            //                       fontSize: 20.0,
-            //                     ),
-            //                   ),
-            //                 ),
-            //                 style: ElevatedButton.styleFrom(
-            //                   primary: Colors.red,
-            //                   shape: RoundedRectangleBorder(
-            //                     borderRadius: BorderRadius.circular(20.0),
-            //                   ),
-            //                 ),
-            //               ),
-            //             ],
-            //           ),
-            //         ),
-            //       ),
-            //     ),
-            //   ),
-            // ),
+            SafeArea(
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white70,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20.0),
+                      ),
+                    ),
+                    width: width * 0.9,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            'Places',
+                            style: TextStyle(fontSize: 20.0),
+                          ),
+                          SizedBox(height: 10),
+                          _textField(
+                              label: 'Start',
+                              hint: 'Choose starting point',
+                              prefixIcon: Icon(Icons.looks_one),
+                              suffixIcon: IconButton(
+                                icon: Icon(Icons.my_location),
+                                onPressed: () {
+                                  startAddressController.text = _currentAddress;
+                                  _startAddress = _currentAddress;
+                                  
+                                },
+                              ),
+                              controller: startAddressController,
+                              focusNode: startAddressFocusNode,
+                              width: width,
+                              locationCallback: (String value) {
+                                setState(() {
+                                  _calculateDistance();
+                                  _startAddress = value;
+                                });
+                              }),
+                          SizedBox(height: 10),
+                          _textField(
+                              label: 'Destination',
+                              hint: 'Choose destination',
+                              prefixIcon: Icon(Icons.looks_two),
+                              controller: destinationAddressController,
+                              focusNode: desrinationAddressFocusNode,
+                              width: width,
+                              locationCallback: (String value) {
+                                setState(() {
+                                  _calculateDistance();
+                                  _destinationAddress = value;
+                                });
+                              }),
+                          SizedBox(height: 10),
+                          Visibility(
+                            visible: _placeDistance == null ? false : true,
+                            child: Text(
+                              'DISTANCE: $_placeDistance km',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          ElevatedButton(
+                            onPressed: (){
+                              
+                              setState(() {
+                                    _startAddress = startAddressController.text;
+                                    print(_startAddress);
+                                    print(_destinationAddress);
+                                  });
+                              },
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.red,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
             // Show current location button
             SafeArea(
               child: Align(
